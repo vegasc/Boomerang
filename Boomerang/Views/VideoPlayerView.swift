84fab98979fd8ab6001b17a5dace5974708120cc
Aleksey Robul <alekseyrobul@gmail.com>
2018-08-09 13:57:14 +0300

    Added: Blur view

--- Boomerang/Views/VideoPlayerView.swift
+++ Boomerang/Views/VideoPlayerView.swift
@@ -11,17 +11,20 @@ import AVKit
 
 class VideoPlayerView: UIView {
     
+    // MARK: UI
+    lazy var blurView: UIView = {
+        return UIVisualEffectView(effect: UIBlurEffect(style: .light))
+    }()
+    /**
+     Uses as background video playback behind blurView
+     */
+    var playerLayerBlur = AVPlayerLayer()
+    var playerLayer = AVPlayerLayer()
+    
     // MARK: Proporties
     var player = AVPlayer()
-    var playerController = AVPlayerViewController()
-    /**
-     Uses for background video playback in blurView
-     */
-    lazy var playerLayer: AVPlayerLayer = {
-        return AVPlayerLayer(player: self.player)
-    }()
     
-    // MARK: Lifecycle funcs
+    // MARK: Override funcs
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -31,28 +34,36 @@ class VideoPlayerView: UIView {
         super.init(coder: aDecoder)
     }
     
+    override func layoutSubviews() {
+        playerLayer.frame = self.bounds
+        playerLayerBlur.frame = self.bounds
+    }
+    
     // MARK: Funcs
     func setVideo(withUrl url:URL){
-        self.player = AVPlayer(url: url)
-        self.playerController.player = self.player
-        self.playerController.player?.play()
+        self.player.replaceCurrentItem(with: AVPlayerItem(url: url))
+        self.player.play()
     }
 }
 
 extension VideoPlayerView {
     func setupUI() {
-        self.playerController.view.frame = self.frame
-        self.playerController.showsPlaybackControls = false
-        self.playerController.view.backgroundColor = .lightGray
-        addSubview(self.playerController.view)
+        self.backgroundColor = .lightGray
         
-        let blurView = UIView()
-        addSubview(blurView)
+        playerLayerBlur.player = self.player
+        playerLayerBlur.videoGravity = .resizeAspectFill
+        layer.addSublayer(playerLayerBlur)
+        
+        self.addSubview(blurView)
         blurView.translatesAutoresizingMaskIntoConstraints = false
         blurView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
         blurView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
         blurView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 0).isActive = true
         blurView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
+        blurView.alpha = 1.0
+        
+        playerLayer.player = self.player
+        layer.addSublayer(playerLayer)
     }
 }
 
